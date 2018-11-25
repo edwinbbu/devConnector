22361d3b4090027698021908b5dc8749e15b918a
Edwin Babu <edwinbbu@gmail.com>
2018-11-25 18:05:05 +0530

    started user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.json({ test: "test" });
+const User = require("../../models/User");
+
+// Register User
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
 });
 
 module.exports = router;
