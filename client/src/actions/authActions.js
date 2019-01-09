9f203c16f40b7119ea83899e9fe721d8410c5a8d
edwinbbu <edwinbbu@gmail.com>
2019-01-09 18:59:17 +0530

    started on login flow

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
 //Register user
 export const registerUser = (userData, history) => dispatch => {
   axios
@@ -14,4 +17,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+//Login - Get User Token
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const token = res.data;
+      //set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      //set token to Auth header
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+// set logged in user
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -127,9 +127,9 @@ class Register extends Component {
   }
 }
 Register.propTypes = {
-  registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  registerUser: PropTypes.func.isRequired
+  // auth: PropTypes.object.isRequired,
+  // errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
